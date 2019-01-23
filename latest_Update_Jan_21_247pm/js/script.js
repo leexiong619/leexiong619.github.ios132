11610b0040437d74464e3122635d4d83cc7f31cb
leexiong619 <lxiong9324@yahoo.com>
2019-01-22 18:36:35 -0800

    Add files via upload

---
+++ latest_Update_Jan_21_247pm/js/script.js
@@ -0,0 +1,18 @@
+/*eslint-env browser*/
+
+/*Submit Button Alert*/
+/*function Warn(){
+    alert ("submission received");
+    document.write ("Your submission was received");
+}
+*/
+
+/*var btn = window.document.getElementById(submitForm);
+btn.onclick = function(){
+    window.alert("Your submission was received");
+}*/
+
+function showAlert(){
+    var myText = "Your information was received and I'll get back to you in  to 3 days ";
+    alert(myText); 
+}
